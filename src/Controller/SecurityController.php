5626066713c51efa04e03ddf976b8de9e83d0cc0
mac <mac@MacBook-Pro-de-mac.local>
2020-12-08 17:35:22 +0100

    Add registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,9 +15,10 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
+            $this->addFlash('error', 'Already logged in');
             return $this->redirectToRoute('app-home');
          }
-
+         
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         // last username entered by the user
